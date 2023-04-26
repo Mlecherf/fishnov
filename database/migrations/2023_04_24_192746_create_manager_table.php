ac3cabbff9168aa6f3d2b50e4bc0a5253c11466a
TMEIGNIER <tanguy.meignier@ynov.com>
2023-04-26 21:54:04 +0200

    initial commit

---
+++ database/migrations/2023_04_24_192746_create_manager_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateManagerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('manager', function (Blueprint $table) {
+            $table->id('id_manager');
+            $table->foreign('id_company')->references('id_company')->on('company')->onDelete('cascade');
+            $table->string('name_manager');
+            $table->string('password_manager');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('manager');
+    }
+}
