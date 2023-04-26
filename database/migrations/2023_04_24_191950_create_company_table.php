7a36f06c6d40047a0f5ed188a21d1a1f6c2d647c
TMEIGNIER <tanguy.meignier@ynov.com>
2023-04-26 21:49:33 +0200

    test

---
+++ database/migrations/2023_04_24_191950_create_company_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompanyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company', function (Blueprint $table) {
+            $table->id('id_company');
+            $table->string('name_company');
+            $table->string('name_admin_company');
+            $table->string('password_admin_company');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company');
+    }
+}
