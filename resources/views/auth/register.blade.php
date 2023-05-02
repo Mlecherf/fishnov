ca5f54a06b184ef4b88744bdd512b28d84956e0b
Mlecherf <mathis.lecherf@gmail.com>
2023-05-02 12:22:44 +0200

    :zap: Login/Auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,18 +12,39 @@
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
-            <!-- Name -->
+            <!-- First Name -->
             <div>
-                <x-label for="name" :value="__('Name')" />
+                <x-label for="first_name" :value="__('First Name')" />
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+                <x-input id="first_name" class="block mt-1 w-full" type="text" name="first_name" :value="old('first_name')" required autofocus />
+            </div>
+
+            <!-- Last Name -->
+            <div class="mt-4">
+                <x-label for="last_name" :value="__('Last Name')" />
+
+                <x-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')" required autofocus />
             </div>
 
             <!-- Email Address -->
             <div class="mt-4">
                 <x-label for="email" :value="__('Email')" />
-
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+            </div>
+
+            <!-- Type -->
+            <div class="mt-4">
+                <x-label for="type" :value="__('Type')" />
+                <select name="type" id="type" onchange="changeRegistrationVisibility()">
+                    <option value="manager">Manager</option>
+                    <option value="trawler">Trawler</option>
+                </select>
+            </div>
+
+            <!-- Registration trawler -->
+            <div class="mt-4" id="registration_trawler_div" hidden="true">
+                <x-label for="registration_trawler" :value="__('Registration Trawler')" />
+                <x-input id="registration_trawler_input" class="block mt-1 w-full" type="text" name="registration_trawler" :value="old('registration_trawler')" required />
             </div>
 
             <!-- Password -->
@@ -57,3 +78,16 @@
         </form>
     </x-auth-card>
 </x-guest-layout>
+
+<script>
+    function changeRegistrationVisibility(caller){
+        let element = document.getElementById("registration_trawler_div");
+        let value = document.getElementById("type").value;
+        if(value == "trawler") {
+            element.hidden = false;
+        } else {
+            document.getElementById("registration_trawler_input").value = "";
+            element.hidden = true;
+        }
+    }
+</script>
