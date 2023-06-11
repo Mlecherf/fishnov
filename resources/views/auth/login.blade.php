b3490d04c267127d3aabdcee0915fb15c8a0f1b8
Mlecherf <mathis.lecherf@gmail.com>
2023-06-11 21:34:51 +0200

    :zap: Added login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,55 +1,190 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+        <title>Fishnov - Login</title>
+        <style>
+            body {
+                background-color: #ABBBCC;
+            }
+            .sub-div {
+                margin: auto;
+                width: fit-content;
+                padding: 10px;
+                margin-top: 50px;
+                display: flex;
+                position: relative;
+                align-items: middle;
+                text-align: center;
+            }
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+            
+            .sub-div button {
+                background-color: #DBDEE2; /* Green */
+                color: white;
+                padding: 15px 128px;
+                font-size: 39px;
+                color: #12507C;
+                text-decoration: none;
+                border-radius: 8px;
+            }
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+            .sub-div input {
+                background-color: #DBDEE2; /* Green */
+                color: white;
+                padding: 15px 128px;
+                font-size: 39px;
+                text-align: center;
+                color: #12507C;
+                text-decoration: none;
+                border-radius: 8px;
+            }
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+            .sub-div button:hover {
+                background-color: #12507C;
+                color:#DBDEE2;
+            }
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            .sub-div a {
+                font-size: 20px;
+                font-family: Arial, sans-serif;
+                color: #12507C;
+            }
+            .text-div {
+                margin: auto;
+                width: fit-content;
+            }
+
+            .text-div h1 {
+                font-size: 64px;
+                font-family: Arial, Helvetica, sans-serif;
+                text-align: center;
+                color: #12507C;
+                text-decoration: none;
+                display: inline-block;
+                border-radius: 8px;
+            }
+            
+            .main-div img {
+                width: 10%;
+            }
+
+            ::selection{background-color: salmon; color: white;}
+            .parallax > use{
+                animation:move-forever 12s linear infinite;
+                &:nth-child(1){animation-delay:-2s;}
+                &:nth-child(2){animation-delay:-2s; animation-duration:5s}
+                &:nth-child(3){animation-delay:-4s; animation-duration:3s}
+            }
+
+            @keyframes move-forever{
+                0%{transform: translate(-90px , 0%)}
+                100%{transform: translate(85px , 0%)} 
+            }
+
+            .editorial {
+                display: block;
+                bottom: 0;
+                margin-top: 9%;
+            }
+
+            .login-errors-label {
+                background-color: #a11300; /* Green */
+                padding: 15px 128px;
+                font-size: 30px;
+                font-family: Arial, sans-serif;
+                text-align: center;
+                color: rgb(35, 35, 35);
+                text-decoration: none;
+                border-radius: 8px;
+            }
+
+            .login-errors {
+                background-color: #a11300; /* Green */
+                padding: 15px 128px;
+                font-size: 30px;
+                font-family: Arial, sans-serif;
+                text-align: center;
+                color: rgb(0, 0, 0);
+                text-decoration: none;
+                border-radius: 8px;
+            }
+
+            a { color: inherit; } 
+            a:link { text-decoration: none; }
+            a:visited { text-decoration: none; }
+            a:hover { text-decoration: none; }
+            a:active { text-decoration: none; }
+            
+            span {
+                display:block;
+            }
+        </style>
+
+    </head>
+    <body class="antialiased">
+        <div class="main-div">
+            <img src="{{ asset('img/fish.png') }}" alt="tag"  />
+            <div class="text-div">
+                <h1>Login</h1>
             </div>
+            <!-- Session Status -->
+            <x-auth-session-status class="mb-4" :status="session('status')" />
+            <!-- Validation Errors -->
+            <x-auth-validation-errors class="mb-4" :errors="$errors" />
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="sub-div">
+                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" placeholder="Email" required autofocus />
+                </div>
+                
+                <div class="sub-div">
+                    <x-input id="password" class="block mt-1 w-full"
+                                    type="password"
+                                    name="password"
+                                    required autocomplete="current-password"
+                                    placeholder="Password" />
+                </div>
+                
+                <div class="sub-div">
+                    <x-button class="ml-3">
+                        {{ __('Login') }}
+                    </x-button>
+                </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
+                    <div class="sub-div">
+                        <a href="{{ route('password.request') }}">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                    </div>
                 @endif
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+            </form>
+
+
+            <svg class="editorial"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                viewBox="0 24 150 28"
+                preserveAspectRatio="none">
+            <defs>
+            <path id="gentle-wave"
+            d="M-160 44c30 0 
+                58-18 88-18s
+                58 18 88 18 
+                58-18 88-18 
+                58 18 88 18
+                v44h-352z" />
+            </defs>
+            <g class="parallax">
+            <use xlink:href="#gentle-wave" x="50" y="0" fill="#4579e2"/>
+            <use xlink:href="#gentle-wave" x="50" y="3" fill="#3461c1"/>
+            <use xlink:href="#gentle-wave" x="50" y="6" fill="#2d55aa"/>  
+            </g>
+            </svg>
+                
+        </div>
+    </body>
+</html>
--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -2,14 +2,15 @@
 
 @if ($errors->any())
     <div {{ $attributes }}>
-        <div class="font-medium text-red-600">
+        <div class="font-medium text-red-600 login-errors-label">
             {{ __('Whoops! Something went wrong.') }}
+
+            <ul class="mt-3 list-disc list-inside text-sm text-red-600 login-errors">
+                @foreach ($errors->all() as $error)
+                    {{ $error }}
+                @endforeach
+            </ul>
         </div>
 
-        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
     </div>
 @endif
