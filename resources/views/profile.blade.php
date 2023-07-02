3efb73e1c5be487b97fb56101d8237b9d5f16c14
Mlecherf <mathis.lecherf@gmail.com>
2023-07-02 13:17:36 +0200

    :zap: profile page for trawler fixed

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -29,6 +29,27 @@
                             <x-input id="email" type="email" name="email" value="{{auth()->user()->email}}" placeholder="{{auth()->user()->email}}" style="background: transparent; border: none;" />
                         </div>
                     </div>
+                    @if (auth()->user()->type=="trawler")
+                        <div class="main-div">
+                            <div class="label-modify">
+                                Registration :
+                                <x-input id="registration_trawler" type="registration_trawler" name="registration_trawler" value="{{auth()->user()->registration_trawler}}" placeholder="{{auth()->user()->registration_trawler}}" style="background: transparent; border: none;" />
+                            </div>
+                        </div>
+                        <div class="main-div">
+                            <div class="label-modify">
+                                Company Token :
+                                @php
+                                    $company  = App\Models\Company::where('id_company',auth()->user()->id_company)->first();
+                                @endphp
+                                @if ($company==null)
+                                    <x-input id="token_company" type="token_company" name="token_company" value="" placeholder="" style="background: transparent; border: none;" />
+                                @else 
+                                    <x-input id="token_company" type="token_company" name="token_company" value="{{$company->token_company}}" placeholder="{{$company->token_company}}" style="background: transparent; border: none;" />       
+                                @endif
+                            </div>
+                        </div>
+                    @endif
                     <div class="main-div">
                         <div class="button-modify">
                             <x-button type="submit">Modifier</x-button>
